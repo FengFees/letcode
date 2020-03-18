675b72431c0046d56046877f72e9c602b93fb19d
fee <502361391@qq.com>
2020-03-19 00:25:24 +0800

    1160 836

--- 回溯/splitIntoFibonacci842.java
+++ 回溯/splitIntoFibonacci842.java
@@ -1,4 +1,92 @@
 package com.回溯;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 842. 将数组拆分成斐波那契序列
+ * 给定一个数字字符串 S，比如 S = "123456579"，我们可以将它分成斐波那契式的序列 [123, 456, 579]。
+ *
+ * 形式上，斐波那契式序列是一个非负整数列表 F，且满足：
+ *
+ * 0 <= F[i] <= 2^31 - 1，（也就是说，每个整数都符合 32 位有符号整数类型）；
+ * F.length >= 3；
+ * 对于所有的0 <= i < F.length - 2，都有 F[i] + F[i+1] = F[i+2] 成立。
+ * 另外，请注意，将字符串拆分成小块时，每个块的数字一定不要以零开头，除非这个块是数字 0 本身。
+ *
+ * 返回从 S 拆分出来的所有斐波那契式的序列块，如果不能拆分则返回 []。
+ *
+ * 示例 1：
+ *
+ * 输入："123456579"
+ * 输出：[123,456,579]
+ * 示例 2：
+ *
+ * 输入: "11235813"
+ * 输出: [1,1,2,3,5,8,13]
+ * 示例 3：
+ *
+ * 输入: "112358130"
+ * 输出: []
+ * 解释: 这项任务无法完成。
+ * 示例 4：
+ *
+ * 输入："0123"
+ * 输出：[]
+ * 解释：每个块的数字不能以零开头，因此 "01"，"2"，"3" 不是有效答案。
+ * 示例 5：
+ *
+ * 输入: "1101111"
+ * 输出: [110, 1, 111]
+ * 解释: 输出 [11,0,11,11] 也同样被接受。
+ * 提示：
+ *
+ * 1 <= S.length <= 200
+ * 字符串 S 中只含有数字。
+ */
+//回溯加剪枝
 public class splitIntoFibonacci842 {
+    static class Solution {
+        public static List<Integer> splitIntoFibonacci(String S) {
+            List<Integer> result = new ArrayList<>();
+            if (S.charAt(0) == '0') return result; //判断开头是否为0
+            if (backTrack(result,0,S)) return result;
+            return result;
+        }
+
+        public static boolean backTrack(List<Integer> result , int index , String S ){
+            if (index == S.length() && result.size()>2) return true;
+
+            for (int i=index ; i<S.length() ; i++ ){
+                String segment = S.substring(index,i+1);
+                //剪枝部分
+                //如果超过数值范围
+                if (Long.parseLong(segment) > Integer.MAX_VALUE) break;
+                //开头为0 可以不判断单独是否为0的状况，往下递归依旧可以剪枝
+                if (!"0".equals(segment) && segment.startsWith("0")) break;
+                //判断是否为fibonacci数列
+                if (judge(Integer.valueOf(segment),result)){
+                    result.add(Integer.valueOf(segment));
+                    //递归
+                    if (backTrack(result,i+1,S)) return true;
+                    //回溯
+                    result.remove(result.size()-1);
+                }
+
+            }
+            return false;
+        }
+
+        private static boolean judge(Integer valueOf, List<Integer> result) {
+            if (result.size()<2 ) return true;
+
+            return valueOf == result.get(result.size() - 1) + result.get(result.size() - 2);
+        }
+
+
+        public static void main(String[] args) {
+            String S="1011";
+            System.out.println(splitIntoFibonacci(S));
+        }
+    }
 }
--- 面试题/compressString0106.java
+++ 面试题/compressString0106.java
@@ -1,4 +1,54 @@
 package com.面试题;
 
+/**
+ * 面试题 01.06. 字符串压缩
+ * 字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。
+ *
+ * 示例1:
+ *
+ *  输入："aabcccccaaa"
+ *  输出："a2b1c5a3"
+ * 示例2:
+ *
+ *  输入："abbccd"
+ *  输出："abbccd"
+ *  解释："abbccd"压缩后为"a1b2c2d1"，比原字符串长度更长。
+ * 提示：
+ *
+ * 字符串长度在[0, 50000]范围内。
+ */
 public class compressString0106 {
+    static class Solution {
+        public static String compressString(String S) {
+            if("".equals(S)) return S;
+            int cnt=0;
+            int temp=0;
+            int right=0;
+            String result="";
+            char s = S.charAt(right);
+            while( right<=S.length()){
+                if ( right<S.length() && s == S.charAt(right) ){
+                    right++;
+                    temp++;
+                }else {
+                    cnt+=2;
+                    if (cnt >= S.length())
+                        return S;
+                    result = result+s+temp;
+                    temp=0;
+                    if (right!=S.length())
+                        s = S.charAt(right);
+                    else break;
+                }
+
+            }
+
+            return result;
+        }
+
+        public static void main(String[] args) {
+            String s = "aabcccccaaa";
+            System.out.println(compressString(s));
+        }
+    }
 }
